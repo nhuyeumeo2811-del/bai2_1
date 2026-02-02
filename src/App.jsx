446a091293e5a715ef68aadd26d0cd98a7a323dc
nhuyeumeo2811-del <nhuyeumeo2811@gmail.com>
2026-02-02 08:30:07 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     <>
       <div style={{padding: "20px"}}>
         <Student name="Nguyễn Ngọc Quỳnh Như" age={17} className="26spit3"/>
+        <Student name="Võ Nguyễn Phương Uyên" age={17} className="26spit3"/>
+        <Student name="Nguyễn Ngọc Quỳnh Nhi" age={17} className="26spit3"/>
       </div>
     </>
   )
